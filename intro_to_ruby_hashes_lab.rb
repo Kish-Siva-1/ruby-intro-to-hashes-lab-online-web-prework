427d49c5a4ab24093369e57d1f2c9841d8620b4d
Kish Siva <rodes88@msn.com>
2019-04-13 03:56:02 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -45,7 +45,8 @@ def monopoly_with_fourth_tier
 	                                                :three_pieces_owned => 100, 
 	                                                :four_pieces_owned => 200},
 	                            :pieces => {4}
-	          	}
+	          	              }
+        	  }
 end
 
 
