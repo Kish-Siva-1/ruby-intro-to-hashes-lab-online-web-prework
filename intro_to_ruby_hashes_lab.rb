0262fda05ce20e1fafa6778c3dc7ab5a109f5931
Kish Siva <rodes88@msn.com>
2019-04-13 04:00:58 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -13,7 +13,7 @@ end
 def monopoly_with_second_tier
   monopoly = {:railroads => {:names => {}, 
                              :rent_in_dollars => {}, 
-                             :pieces => {4}
+                             :pieces => 4}
                             }
              }
 end
