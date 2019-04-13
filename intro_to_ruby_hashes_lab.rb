0d747cdae0dcc996c713037c7adcd6824551040f
Kish Siva <rodes88@msn.com>
2019-04-13 03:50:01 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -11,8 +11,7 @@ def monopoly
 end
 
 def monopoly_with_second_tier
-  monopoly = {:railroads => {:names => {}, :rent_in_dollars => {}}, :pieces => {4}}
-
+  monopoly = {:railroads => {:names => {}, :rent_in_dollars => {}, :pieces => {4}}}
 end
 
 def monopoly_with_third_tier
